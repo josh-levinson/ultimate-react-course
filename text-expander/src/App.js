da4033d6b540332ccf7e347efe63247e2d1128b5
Josh Levinson <joshua.levinson@gmail.com>
2024-04-18 23:49:07 -0400

    some older stuff. a challenge. some updates. happy thursday

---
+++ text-expander/src/App.js
@@ -0,0 +1,82 @@
+import "./index.css";
+import React, { useState } from "react";
+
+export default function App() {
+  return (
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+    </div>
+  );
+}
+
+const containerStyle = {
+  margin: 10,
+};
+
+function TextExpander({
+  expanded = false,
+  collapsedNumberWords = 10,
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+  buttonColor = "red",
+  children,
+  className,
+}) {
+  const words = children.split(" ");
+  const shortenedWords = words.slice(0, collapsedNumberWords);
+  const shortenedText = shortenedWords.join(" ");
+  const [textExpanded, setTextExpanded] = useState(expanded);
+  const [displayText, setDisplayText] = useState(
+    expanded ? children : shortenedText
+  );
+
+  function handleExpand(textExpanded) {
+    setTextExpanded(textExpanded);
+    setDisplayText(textExpanded ? children : shortenedText);
+  }
+
+  return (
+    <div style={containerStyle} className={className}>
+      <span style={{ marginRight: 5 }}>{displayText}</span>
+      {!textExpanded && (
+        <span style={{ color: buttonColor }} onClick={() => handleExpand(true)}>
+          {expandButtonText}
+        </span>
+      )}
+      {textExpanded && (
+        <span
+          style={{ color: buttonColor }}
+          onClick={() => handleExpand(false)}
+        >
+          {collapseButtonText}
+        </span>
+      )}
+    </div>
+  );
+}
--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
-import StarRating from './StarRating';
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <p>The rating of this movie is {movieRating} stars</p>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} messages={["Horrible", "Not good", "Meh", "Solid", "WOO HOO"]} />
-    <StarRating maxRating={5} color="purple" size={24} />
+    <StarRating
+      maxRating={5}
+      messages={["Horrible", "Not good", "Meh", "Solid", "WOO HOO"]}
+    />
+    <StarRating maxRating={5} color="purple" size={24} defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
-
