9ef850f093fd52d2b6e7fc5498b2ae27b449f211
Josh Levinson <joshua.levinson@gmail.com>
2024-10-31 12:51:49 -0400

    add restarts and emoji scores

--- react-quiz/src/components/FinishScreen.js
+++ react-quiz/src/components/FinishScreen.js
@@ -1,10 +1,31 @@
-export default function FinishScreen({ points, maxPossiblePoints }) {
+export default function FinishScreen({
+  dispatch,
+  points,
+  maxPossiblePoints,
+  highscore,
+}) {
   const percentage = (points / maxPossiblePoints) * 100;
 
+  let emoji;
+  if (percentage === 100) emoji = "🏆";
+  if (percentage >= 80 && percentage < 100) emoji = "🥳";
+  if (percentage >= 50 && percentage < 80) emoji = "😊";
+  if (percentage >= 25 && percentage < 50) emoji = "😐";
+  if (percentage < 25) emoji = "😬";
+
   return (
-    <p className="result">
-      You scored <strong>{points}</strong> out of {maxPossiblePoints} points (
-      {percentage}%)
-    </p>
+    <>
+      <p className="result">
+        {emoji} You scored <strong>{points}</strong> out of {maxPossiblePoints}{" "}
+        points ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">Highscore: {highscore}</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Take Again!
+      </button>
+    </>
   );
 }
