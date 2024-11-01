7cb910025f7ede93538bcde56073848fc509ec5e
Josh Levinson <joshua.levinson@gmail.com>
2024-10-31 22:04:13 -0400

    hooray we're done

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -11,6 +11,8 @@ import Question from "./Question";
 import StartScreen from "./StartScreen";
 import Timer from "./Timer";
 
+const SECONDS_PER_QUESTION = 30;
+
 const initialState = {
   questions: [],
   // 'loading', 'error', 'ready', 'active', 'finished'
@@ -19,7 +21,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
-  secondsRemaining: 1000,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -39,6 +41,7 @@ function reducer(state, action) {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECONDS_PER_QUESTION,
       };
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -75,6 +78,7 @@ function reducer(state, action) {
       return {
         ...state,
         secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
       };
     default:
       throw new Error("Action unknown");
--- react-quiz/src/components/Timer.js
+++ react-quiz/src/components/Timer.js
@@ -1,11 +1,22 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+  const minutes = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
   useEffect(() => {
-    setInterval(() => dispatch("tick"), 1000);
+    const id = setInterval(() => dispatch({ type: "tick" }), 1000);
+
+    return () => clearInterval(id);
   }, [dispatch]);
 
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {minutes < 10 && "0"}
+      {minutes}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
 
 export default Timer;
