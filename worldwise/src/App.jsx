bc06490ef815e433878a221577c5d6cea8e64b2d
Josh Levinson <joshua.levinson@gmail.com>
2024-11-08 14:03:15 -0500

    just enjoying a nice day off

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>Worldwide</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
