92dac8f8813062b8919439f8042b6e47bd60767d
Joshua Levinson <jlevinson@linksquares.com>
2024-11-15 16:33:08 -0500

    finish lecture

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -2,10 +2,16 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) {
     return <Spinner />;
+  }
+  if (cities.length === 0) {
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
   }
 
   if (!cities.length)
